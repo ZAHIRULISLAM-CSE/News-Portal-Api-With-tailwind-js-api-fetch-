376de585e8a13511b91c3fbdfd4f9fa6088dec32
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-03-02 12:22:48 +0600

    tranding and not tranding icon added from api

--- news.js
+++ news.js
@@ -1,12 +1,13 @@
 // Fetch news title
-const fetchNewsTitle=(defid)=>{
+const fetchNewsTitle=()=>{
     const url="https://openapi.programming-hero.com/api/news/categories";
     fetch(url)
     .then(response => response.json())
-    .then(data =>  displayTitle(data.data.news_category,defid))
+    .then(data =>  displayTitle(data.data.news_category))
 }
 
 const displayTitle=(data)=>{
+    // console.log(data)
     data.forEach(singleData =>{
         // console.log(singleData)
         const {category_name,category_id}=singleData;
@@ -31,8 +32,10 @@ const displayNews=(data)=>{
         let {title,details,image_url,author,total_view,rating,_id}=singleNews;
         let {name, published_date,img}=author;
         let {number}=rating;
+        const tranding=singleNews.others_info.is_trending;
+        console.log(tranding);
         // console.log(number);
-        // console.log(singleNews)
+        console.log(singleNews)
         details=details.slice(0,200)
         const newsContainer=document.getElementById('news-container');
         newsContainer.innerHTML +=`
@@ -42,6 +45,7 @@ const displayNews=(data)=>{
                     </div>
                     <div class="flex-1 card-body">
                         <h2 class="card-title">${title}</h2>
+                        <div class="badge badge-secondary">${tranding ? "Tranding" : "Not Tranding"}</div>
                         <p class="mt-3">${details}</p>
                         <div class="lg:flex items-center justify-between">
                             <div class="flex gap-4 items-center">
@@ -71,7 +75,6 @@ const displayNews=(data)=>{
 }
 
 const star=totalStar=>{
-    console.log(totalStar)
     let starContainer='';
     for(let i=1;i<=Math.floor(totalStar);i++){
     //    starContainer=document.getElementById("star");
